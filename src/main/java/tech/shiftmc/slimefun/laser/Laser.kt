3186b54b0b2d93e47c4aafbc14a7159c280b4a41
ShiftSad <rosalimavictor@gmail.com>
2023-04-15 20:34:59 -0300

    🎉 Primeiro Commit!

---
+++ src/main/java/tech/shiftmc/slimefun/laser/Laser.kt
@@ -0,0 +1,14 @@
+package tech.shiftmc.slimefun.laser
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Laser : JavaPlugin() {
+    
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+    
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
